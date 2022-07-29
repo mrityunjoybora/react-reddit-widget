0db7b231305d4c80805d60ce9cbe6b0b4f87312a
Mrityunjoy Bora <mjborabiz@gmail.com>
2022-07-29 14:39:13 +0530

    .

--- src/index.js
+++ src/index.js
@@ -3,17 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-const WidgetDivs = document.querySelectorAll(".reddit_widget");
-
-WidgetDivs.forEach((Div) => {
-  const root = ReactDOM.createRoot(Div);
-  root.render(
-    <React.StrictMode>
-      <App domElement={Div}/>
-    </React.StrictMode>
-  );
-});
-
-
-
-
+const root = ReactDOM.createRoot(document.getElementById("reddit_widget"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
